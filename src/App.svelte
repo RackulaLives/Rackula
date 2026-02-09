c958d16a3e9fb80f40a1e93a4aae9cac5dd8a031
ggfevans <hi@gvns.ca>
2026-02-09 00:07:07 -0800

    feat: add mobile floating undo/redo controls
    Fixes #1046

--- src/App.svelte
+++ src/App.svelte
@@ -28,6 +28,7 @@
   import DeviceDetails from "$lib/components/DeviceDetails.svelte";
   import MobileFileSheet from "$lib/components/MobileFileSheet.svelte";
   import MobileBottomNav from "$lib/components/mobile/MobileBottomNav.svelte";
+  import MobileHistoryControls from "$lib/components/mobile/MobileHistoryControls.svelte";
   import RackIndicator from "$lib/components/mobile/RackIndicator.svelte";
   import RackEditSheet from "$lib/components/RackEditSheet.svelte";
   import MobileViewSheet from "$lib/components/mobile/MobileViewSheet.svelte";
@@ -1439,6 +1440,8 @@
     <RackIndicator />
 
     <main class="app-main" class:mobile={viewportStore.isMobile}>
+      <MobileHistoryControls />
+
       {#if !viewportStore.isMobile}
         <PaneGroup
           direction="horizontal"
